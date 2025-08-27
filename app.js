05069373403e538019e8b722493515a80b62f839
Santiago Ponce <queenero12@gmail.com>
2025-08-27 18:24:37 -0300

    agregando función de la lista

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ let listaAmigos = [];
 
 
 function agregarAmigo(){
-
+// comando agregar amigos
     const amigosUsuario = document.getElementById("amigo");
 
     const valor = amigosUsuario.value;
    
     console.log(valor);
 
+//comando lista
+    const nuevoLi = document.createElement("li");
+
+    nuevoLi.textContent = valor;
+
+    const lista = document.getElementById("listaAmigos");
+
+    lista.appendChild(nuevoLi);
+
+        amigosUsuario.value = "";
+    
     
     if (valor){
 
@@ -21,6 +32,8 @@ else if (isNaN){
 alert("Por favor, inserte un nombre.")
 
 }
+
+
 }
 
 
